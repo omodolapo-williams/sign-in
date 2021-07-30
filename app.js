a120b7d49aa7e18c9f9709c71052f07433ba6f75
omodolapo-williams <88205624+omodolapo-williams@users.noreply.github.com>
2021-07-30 17:18:49 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -80,6 +80,70 @@ passport.deserializeUser(function (id, done){
   })
 }))
 
+function isLoggedIn(req, res, next) {
+	if (req.isAuthenticated()) return next();
+	res.redirect('/login');
+}
+
+function isLoggedOut(req, res, next) {
+	if (!req.isAuthenticated()) return next();
+	res.redirect('/');
+}
+
+// ROUTES
+app.get('/', isLoggedIn, (req, res) => {
+	res.render("index", { title: "Home" });
+});
+
+app.get('/about', (req, res) => {
+	res.render("index", { title: "About" });
+});
+
+app.get('/login', isLoggedOut, (req, res) => {
+	const response = {
+		title: "Login",
+		error: req.query.error
+	}
+
+	res.render('login', response);
+});
+
+app.post('/login', passport.authenticate('local', {
+	successRedirect: '/',
+	failureRedirect: '/login?error=true'
+}));
+
+app.get('/logout', function (req, res) {
+	req.logout();
+	res.redirect('/');
+});
+
+// Setup our admin user
+app.get('/setup', async (req, res) => {
+	const exists = await User.exists({ username: "admin" });
+
+	if (exists) {
+		res.redirect('/login');
+		return;
+	};
+
+	bcrypt.genSalt(10, function (err, salt) {
+		if (err) return next(err);
+		bcrypt.hash("pass", salt, function (err, hash) {
+			if (err) return next(err);
+
+			const newAdmin = new User({
+				username: "admin",
+				password: hash
+			});
+
+			newAdmin.save();
+
+			res.redirect('/login');
+		});
+	});
+});
+
 // renders from a template from a view folder
 app.get('/', (req, res) =>{
   res.render('index', {title: 'Home'})
